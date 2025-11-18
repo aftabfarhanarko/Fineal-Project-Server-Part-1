68e70e5ca961f509821b033e621d9536c6f2168d
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-18 15:45:25 +0600

    whufg

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Server COde Running Now");
+const express = require('express')
+const app = express()
+const cors = require("cors")
+const port = 3000
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
